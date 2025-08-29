c1990eba693fa935fe3fd60ca47e88bfd28f0022
rahul0121997 <rahul0121997@gmail.com>
2025-08-29 09:59:34 +0530

    adding two python file

---
+++ pattern.py
@@ -0,0 +1,4 @@
+row = 6
+
+for i in range(7):
+    print("*" * (i - 1) + " " * (7 - i) )
--- second.py
+++ second.py
@@ -1,3 +1,4 @@
 print("hello backend developer")
 print("hello frontend developer")
 print("hello fullstack developer")
+print("hello flutter developer")
